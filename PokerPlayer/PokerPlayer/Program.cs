0740e210b24f92c0eb078579074a752ab2969b07
PatrickSHYee <patrickshyee@gmail.com>
2015-02-14 23:11:07 -0700

    Put comments in....

--- PokerPlayer/PokerPlayer/Program.cs
+++ PokerPlayer/PokerPlayer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,32 +15,19 @@ namespace PokerPlayer
             PokerPlayer player = new PokerPlayer();
             List<Card> testHand = new List<Card>();
             dealerDeck = new Deck();
-            // lets shuffle the deck 10 times
-            for (int s = 0; s < 10; s++)
+            // lets shuffle the deck 100 times
+            for (int s = 0; s < 100; s++)
             {
                 dealerDeck.Shuffle();
             }
 
-            player.DrawHand(dealerDeck.Deal(5));
-            player.ShowHand();
+            for (int i = 0; i < 5; i++)
+            {
+                player.DrawHand(dealerDeck.Deal(5));
+                player.ShowHand();
+            }
             Console.ReadKey();
         }
-    }
-
-    /// <summary>
-    /// the suits for the cards to have
-    /// </summary>
-    public enum Suit
-    {
-        Heart, Diamond, Club, Spade
-    }
-
-    /// <summary>
-    /// The ranking for the cards
-    /// </summary>
-    public enum Rank
-    {
-        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
     }
 
     /// <summary>
@@ -63,7 +50,7 @@ namespace PokerPlayer
             get { return _listOfCards; }
             set { _listOfCards = value; }
         }
-        
+
         // Rank of hand that player holds
         private HandType _handRank;
         public HandType HandRank { set { _handRank = value; } get { return _handRank; } }
@@ -76,7 +63,8 @@ namespace PokerPlayer
         /// <summary>
         /// nothing is created, but for the player
         /// </summary>
-        public PokerPlayer() {
+        public PokerPlayer()
+        {
             HandRank = HandType.HighCard;
         }
 
@@ -144,7 +132,7 @@ namespace PokerPlayer
         /// </summary>
         public void ShowHand()
         {
-            ListOfCards.ForEach(x=>Console.Write("{0}, ", x.GetCard()));
+            ListOfCards.ForEach(x => x.ShowCard());
             Console.WriteLine("\n{0}", MyHand);
         }
 
@@ -160,40 +148,57 @@ namespace PokerPlayer
         /// <summary>
         /// Finds the two pair in a hand
         /// </summary>
-        /// <returns></returns>
+        /// <returns>True if the 2 pairs</returns>
         public bool HasTwoPair()
         {
-            return ListOfCards.GroupBy(x=>x.Ranked).Count(x=>x.Count() >= 2 ) == 2;
+            return ListOfCards.GroupBy(x => x.Ranked).Count(x => x.Count() >= 2) == 2;
         }
+        
+        /// <summary>
+        /// Checks for a three of a kind
+        /// </summary>
+        /// <returns>boolean return</returns>
         public bool HasThreeOfAKind()
         {
-            if (ListOfCards.GroupBy(x=>x.Ranked).Any(x=>x.Count() == 3))
+            if (ListOfCards.GroupBy(x => x.Ranked).Any(x => x.Count() == 3))
             {
                 return true;
             }
             return false;
         }
+
+        /// <summary>
+        /// Checks if the hand is a straight. Even checks if A, 2, 3, 4, 5 is a straight
+        /// </summary>
+        /// <returns>True if the has a straight</returns>
         public bool HasStraight()
         {
             bool itsGood = true;
-            List<Card> tempList = ListOfCards.OrderBy(x=>x.Ranked).ToList();
+            List<Card> tempList = ListOfCards.OrderBy(x => x.Ranked).ToList();
+            int count = tempList.Count;
 
             // checks if is A, 2, 3, 4, 5 straight.
             // remember the ace is a high cardeturn itsGood;
-            if (ListOfCards.OrderBy(x => x.Ranked).Last().Ranked == Rank.Ace && tempList[0].Ranked == Rank.Two && !itsGood)
+            if (tempList.Last().Ranked == Rank.Ace && tempList.First().Ranked == Rank.Two)
             {
                 itsGood = true;
+                count--; // make sure that the last element does gets check as well.
             }
-            
-            for (int i = 0; i < tempList.Count - 1 ; i++)
+
+            for (int i = 0; i < count - 1; i++)
             {
-                if (tempList[i + 1].Ranked-1 != tempList[i].Ranked)
+                if (tempList[i + 1].Ranked - 1 != tempList[i].Ranked)
                 {
                     itsGood = false;
                 }
             }
             return itsGood;
         }
+
+        /// <summary>
+        /// Checks for a flush
+        /// </summary>
+        /// <returns>boolean value for a flush</returns>
         public bool HasFlush()
         {
             if (ListOfCards.Select(x => x.TheSuit).Distinct().Count() == 1)
@@ -202,6 +207,11 @@ namespace PokerPlayer
             }
             return false;
         }
+
+        /// <summary>
+        /// Check if three of a kind and has a pair
+        /// </summary>
+        /// <returns>return true if it three of a kind</returns>
         public bool HasFullHouse()
         {
             if (HasThreeOfAKind() && HasPair())
@@ -210,14 +220,24 @@ namespace PokerPlayer
             }
             return false;
         }
+
+        /// <summary>
+        /// Checks if there is four of a kind and that it's not the same suit
+        /// </summary>
+        /// <returns></returns>
         public bool HasFourOfAKind()
         {
             if (ListOfCards.GroupBy(x => x.Ranked).Any(x => x.Count() == 4) && ListOfCards.Select(x => x.TheSuit).Distinct().Count() >= 4)
             {
                 return true;
             }
-            return false ;
+            return false;
         }
+
+        /// <summary>
+        /// If HasStraight and HasFlush is both true to make the check.
+        /// </summary>
+        /// <returns>it's a big winner</returns>
         public bool HasStraightFlush()
         {
             if (HasStraight() && HasFlush())
@@ -226,6 +246,11 @@ namespace PokerPlayer
             }
             return false;
         }
+
+        /// <summary>
+        /// checks if the hand is a straightFlush and that the first card is a ten.
+        /// </summary>
+        /// <returns>It's money</returns>
         public bool HasRoyalFlush()
         {
             if (HasStraightFlush() && ListOfCards.OrderBy(x => x.Ranked).First().Ranked == Rank.Ten)
@@ -238,17 +263,23 @@ namespace PokerPlayer
     //Guides to pasting your Deck and Card class
 
     //  *****Deck Class Start*****
+    /// <summary>
+    /// The making of a deck of cards
+    /// </summary>
     class Deck
     {
         private List<Card> _deckOfCards;
         public List<Card> DeckOfCards { get { return _deckOfCards; } set { _deckOfCards = value; } }
         private List<Card> _discardedCards = new List<Card>();
         public List<Card> DiscardedCards { get; set; }
-        // a deck has 52 cards of each of suits with ranks
+
+        /// <summary>
+        /// A normal deck of cards has 52 card of each suit with ranks
+        /// </summary>
         public Deck()
         {
             DeckOfCards = new List<Card>();
-            DiscardedCards = new List<Card>(); ;
+            DiscardedCards = new List<Card>();
             for (int r = 2; r <= 14; r++)
             {
                 for (int s = 1; s <= 4; s++)
@@ -258,14 +289,31 @@ namespace PokerPlayer
             }
         }
 
+        /// <summary>
+        /// It creates a number of decks in the deck.
+        /// </summary>
+        /// <param name="howMany">How many decks it needs to be created</param>
         public Deck(int howMany)
         {
+            DeckOfCards = new List<Card>();
+            DiscardedCards = new List<Card>();
             for (int i = 0; i < howMany; i++)
             {
-                new Deck();
+                for (int r = 2; r <= 14; r++)
+                {
+                    for (int s = 1; s <= 4; s++)
+                    {
+                        DeckOfCards.Add(new Card((Rank)r, (Suit)s));
+                    }
+                }
             }
         }
 
+        /// <summary>
+        /// it pulls out how many cards from the deck
+        /// </summary>
+        /// <param name="numOfCards">the number of cards to be pulled</param>
+        /// <returns>A list of cards</returns>
         public List<Card> Deal(int numOfCards)
         {
             List<Card> BigHand = new List<Card>();
@@ -277,6 +325,10 @@ namespace PokerPlayer
             return BigHand;
         }
 
+        /// <summary>
+        /// it discards the list of cards
+        /// </summary>
+        /// <param name="hand"></param>
         public void Discard(List<Card> hand)
         {
             while (hand.Count > 0)
@@ -286,12 +338,15 @@ namespace PokerPlayer
             }
         }
 
+        /// <summary>
+        /// Shuffle the deck
+        /// </summary>
         public void Shuffle()
         {
             List<Card> newDeckOfCards = new List<Card>();
             Random shuffler = new Random();
             // put the cards back to the DeckOfCards from the Discard piles
-            if (DiscardedCards.Count == 0)
+            if (DiscardedCards.Count != 0)
             {
                 while (DiscardedCards.Count > 0)
                 {
@@ -313,16 +368,30 @@ namespace PokerPlayer
     //  *****Deck Class End*******
 
     //  *****Card Class Start*****
-    
+
     // What makes a card?
-	//     A card is comprised of it’s suit and its rank.  Both of which are enumerations.
+    //     A card is comprised of it’s suit and its rank.  Both of which are enumerations.
+    //     These enumerations should be "Suit" and "Rank"
+    public enum Suit
+    {
+        Heart = 1,
+        Diamond, Club, Spade
+    }
+
+    /// <summary>
+    /// the rank for the card
+    /// </summary>
+    public enum Rank
+    {
+        Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+    }
+    // What makes a card?
+    //     A card is comprised of it’s suit and its rank.  Both of which are enumerations.
     //     These enumerations should be "Suit" and "Rank"
     class Card
     {
-        private Suit _theSuit;
-        public Suit TheSuit { get { return _theSuit; } set { _theSuit = value; } }
-        private Rank _ranked;
-        public Rank Ranked { get { return _ranked; } set { _ranked = value; } }
+        public Suit TheSuit { get; set; }
+        public Rank Ranked { get; set; }
 
         public Card(Rank rank, Suit suit)
         {
@@ -332,15 +401,17 @@ namespace PokerPlayer
 
         public Card(int rank, int suit)
         {
-            this.TheSuit = (Suit)Enum.ToObject(typeof(Suit), suit);
-            this.Ranked = (Rank)Enum.ToObject(typeof(Rank), rank);
+            this.TheSuit = (Suit)suit;
+            this.Ranked = (Rank)rank;
         }
 
-        public string GetCard()
+        /// <summary>
+        /// Shows the card
+        /// </summary>
+        public void ShowCard()
         {
-            return this.Ranked + " of " + this.TheSuit;
+            Console.WriteLine("{0} of {1}", this.Ranked, this.TheSuit);
         }
-
     }
     //  *****Card Class End*******class Deck
 }
